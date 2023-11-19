c813860b6887b5eda294f46b3000d69ef9e74945
Kurasao <jamerolenor@gmail.com>
2023-11-19 14:57:50 +0800

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ import Button from "./Button";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useUser } from "@/hooks/useUser";
-import { toast } from "react-hot-toast/headless";
+import { toast } from "react-hot-toast";
 
 interface HeaderProps {
   children: React.ReactNode;
